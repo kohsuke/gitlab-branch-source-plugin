fc2d2621f060195b8d0368cea476f67cc6d4834a
Kohsuke Kawaguchi <kk@kohsuke.org>
2017-02-08 07:24:01 -0800

    This is only useful for me

--- api.sh
+++ api.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -H "PRIVATE-TOKEN: J3_wGdxJqr964Bowozwo" http://gitlab.example.com:8080/api/v3/$1 | json_xs
+curl -v -H "PRIVATE-TOKEN: J3_wGdxJqr964Bowozwo" http://gitlab.example.com:9023/api/v3/$1 | json_xs
